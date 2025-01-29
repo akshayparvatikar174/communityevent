3c0ae8c63d490ed43e05c3f99c535da922099e93
akshayparvatikar174 <apravati@progress.com>
2025-01-29 20:13:18 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,7 @@ service 'nginx' do
   action [:enable, :start]
 end
 
-directory '/var/www/html' do
+directory '/usr/share/nginx/html' do
   owner 'www-data'
   group 'www-data'
   mode '0755'
@@ -14,16 +14,16 @@ directory '/var/www/html' do
 end
 
 execute 'clean_old_files' do
-  command 'rm -rf /var/www/html/*'
-  only_if { Dir.exist?('/var/www/html') }
+  command 'rm -rf /usr/share/nginx/html/*'
+  only_if { Dir.exist?('/usr/share/nginx/html') }
 end
 
 execute 'clone_github_repo' do
-  command 'git clone https://github.com/akshayparvatikar174/HTMLpages.git /var/www/html'
-  not_if { File.exist?('/var/www/html/index.html') }
+  command 'git clone https://github.com/akshayparvatikar174/HTMLpages.git /usr/share/nginx/html'
+  not_if { File.exist?('/usr/share/nginx/html/index.html') }
 end
 
-file '/var/www/html/index.html' do
+file '/usr/share/nginx/html/index.html' do
   owner 'www-data'
   group 'www-data'
   mode '0644'
