3254620fed40a453a550f5eb5a094c74809426c0
akshayparvatikar174 <apravati@progress.com>
2025-03-05 20:44:58 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -54,3 +54,41 @@ file '/home/polyfil/sandbox.html' do
   action :create
   only_if { node['ipaddress'] == '172.31.14.194' }
 end
+
+ruby_block 'update_index_html' do
+  block do
+    index_file = '/var/www/html/index.html'
+    message = case node['ipaddress']
+              when '172.31.1.65'
+                '<p>Welcome to machine 1</p>'
+              when '172.31.14.194'
+                '<p>Welcome to machine 2</p>'
+              else
+                '<p>Unknown Machine</p>'
+              end
+
+    # Read index.html and insert the message inside <body>
+    file_content = File.read(index_file)
+    new_content = file_content.sub('</body>', "#{message}\n</body>")
+
+    File.write(index_file, new_content)
+  end
+  only_if { File.exist?('/var/www/html/index.html') }
+end
+
+file '/var/www/html/machine_info.txt' do
+  content lazy {
+    case node['ipaddress']
+    when '172.31.1.65'
+      'Welcome to Production Machine'
+    when '172.31.14.194'
+      'Welcome to Staging Machine'
+    else
+      'Unknown Machine'
+    end
+  }
+  owner 'www-data'
+  group 'www-data'
+  mode '0644'
+  action :create
+end
