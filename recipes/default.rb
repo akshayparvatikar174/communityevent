270d6ec6e313ff4559602f6ade28ea6f34d7b538
akshayparvatikar174 <apravati@progress.com>
2025-03-05 19:20:29 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -46,7 +46,6 @@ service 'nginx' do
   action :restart
 end
 
-# ✅ Creates sandbox.html only on machine 172.31.14.194
 file '/home/polyfil/sandbox.html' do
   content '<h1>Hello, Chef!</h1>'
   owner 'root'
@@ -54,43 +53,4 @@ file '/home/polyfil/sandbox.html' do
   mode '0644'
   action :create
   only_if { node['ipaddress'] == '172.31.14.194' }
-end
-
-# ✅ Machine-Specific Welcome Message inside index.html
-ruby_block 'update_index_html' do
-  block do
-    index_file = '/var/www/html/index.html'
-    message = case node['ipaddress']
-              when '172.31.1.65'
-                '<p>Welcome to machine 1</p>'
-              when '172.31.14.194'
-                '<p>Welcome to machine 2</p>'
-              else
-                '<p>Unknown Machine</p>'
-              end
-
-    # Read index.html and insert the message inside <body>
-    file_content = File.read(index_file)
-    new_content = file_content.sub('</body>', "#{message}\n</body>")
-
-    File.write(index_file, new_content)
-  end
-  only_if { File.exist?('/var/www/html/index.html') }
-end
-
-file '/var/www/html/machine_info.txt' do
-  content lazy {
-    case node['ipaddress']
-    when '172.31.1.65'
-      'Welcome to Production Machine'
-    when '172.31.14.194'
-      'Welcome to Staging Machine'
-    else
-      'Unknown Machine'
-    end
-  }
-  owner 'www-data'
-  group 'www-data'
-  mode '0644'
-  action :create
 end
